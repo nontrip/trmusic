0adb53d9e4f392d32f43e613d7258ecf7f71adbf
nontrip <alexanderkobrin@iMac-Alexander.local>
2017-01-02 23:12:24 +0300

    iPad with hamb

--- setup.js
+++ setup.js
@@ -1,3 +1,5 @@
+var menubar = false;
+
 $(document).ready(function(){
 
   $(window).scroll(function(){
@@ -36,6 +38,21 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
+    $('#hamb').on('click', function(){
+      if(menubar==false){
+        $('#menubar').css('display', 'block');
+        $(this).animate({'margin-left': $('#menubar').width()}, 900)
+        $('#menubar').animate({'margin-left': 0}, 1000);
+        menubar=true;
+      } else {
+        $(this).animate({'margin-left': 50}, 900)
+        $('#menubar').animate({'margin-left': -$('#menubar').width()}, 1000, function() {
+          $('#menubar').css('display', 'none');
+        });
+        menubar=false;
+      }
+    });
+
   function abso(el) {
     $(el).css({
       height: $(window).height()
@@ -45,11 +62,14 @@ $(document).ready(function(){
   $(window).resize(function() {
     abso($('.slide'));
     abso($('#menubar'));
-    $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2)
-    $('#description').css('margin-left', $(window).width()/2.5+$('#menubar').width()-$('#description').width()/2)
-    $('#description').css('margin-top', $(window).height()/2-$('#slide3_heading').height()-$('#description').height()/2)
-    $('#social_icons').css('margin-top', $(window).height()/2-$('#menu_heading').height()/2-$('#menu_nav').height()/2)
     if($(window).width()>910){
+      $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2)
+      $('#description').css('margin-left', $(window).width()/2.5+$('#menubar').width()-$('#description').width()/2)
+      $('#description').css('margin-top', $(window).height()/2-$('#slide3_heading').height()-$('#description').height()/2)
+      $('#social_icons').css('margin-top', $(window).height()/2-$('#menu_heading').height()/2-$('#menu_nav').height()/2)
+      $('#hamb').css('display', 'none');
+      $('#menubar').css('display', 'block');
+      $('#menubar').css('margin-left', 0);
       $('.album_cover').css('width', $(window).height()/2.5);
       $('.album_cover').css('height', $(window).height()/2.5);
       $('#album_covers').css('margin-left', $(window).width()/2+$('#menubar').width()/2-$('#album_covers').width()/2);
@@ -57,6 +77,13 @@ $(document).ready(function(){
       $('#form-main').css('margin-top', $(window).height()/5-$('#contact_heading').height()-$('#form-main').height());
       $('#form-div').css('margin-left', -$('#menubar').width());
     }else {
+      $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2)
+      $('#description').css('margin-left', $(window).width()/2-$('#description').width()/2)
+      $('#description').css('margin-top', $(window).height()/2-$('#slide3_heading').height()-$('#description').height()/2)
+      $('#social_icons').css('margin-top', $(window).height()/2-$('#menu_heading').height()/2-$('#menu_nav').height()/2)
+      $('#hamb').css('display', 'block');
+      $('#menubar').css('margin-left', -$('#menubar').width());
+      $('#menubar').css('display', 'none');
       $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2);
       $('#heading').css('margin-left', $(window).width()/2-$('#heading').width()/2);
       $('.album_cover').css('width', $(window).height()/3);
@@ -69,19 +96,30 @@ $(document).ready(function(){
 
     abso($('.slide'));
     abso($('#menubar'));
-    $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2);
-    $('#heading').css('margin-left', $(window).width()/2+$('#menubar').width()-$('#heading').width()/2);
-    $('#description').css('margin-left', $(window).width()/2.5+$('#menubar').width()-$('#description').width()/2);
-    $('#description').css('margin-top', $(window).height()/2-$('#slide3_heading').height()-$('#description').height()/2);
-    $('#social_icons').css('margin-top', $(window).height()/2-$('#menu_heading').height()/2-$('#menu_nav').height()/2);
+
     if($(window).width()>910){
+      $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2)
+      $('#description').css('margin-left', $(window).width()/2.5+$('#menubar').width()-$('#description').width()/2)
+      $('#description').css('margin-top', $(window).height()/2-$('#slide3_heading').height()-$('#description').height()/2)
+      $('#social_icons').css('margin-top', $(window).height()/2-$('#menu_heading').height()/2-$('#menu_nav').height()/2)
+      $('#hamb').css('display', 'none');
+      $('#menubar').css('display', 'block');
+      $('#menubar').css('margin-left', 0);
       $('.album_cover').css('width', $(window).height()/2.5);
       $('.album_cover').css('height', $(window).height()/2.5);
       $('#album_covers').css('margin-left', $(window).width()/2+$('#menubar').width()/2-$('#album_covers').width()/2);
       $('#album_covers').css('margin-top', $(window).height()/2-$('#release_heading').height()-$('#album_covers').height()/1.5);
       $('#form-main').css('margin-top', $(window).height()/5-$('#contact_heading').height()-$('#form-main').height());
       $('#form-div').css('margin-left', -$('#menubar').width());
-    } else {
+    }else {
+      $('#heading').css('margin-left', $(window).width()/2-$('#heading').width()/2);
+      $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2)
+      $('#description').css('margin-left', $(window).width()/2.-$('#description').width()/2)
+      $('#description').css('margin-top', $(window).height()/2-$('#slide3_heading').height()-$('#description').height()/2)
+      $('#social_icons').css('margin-top', $(window).height()/2-$('#menu_heading').height()/2-$('#menu_nav').height()/2)
+      $('#hamb').css('display', 'block');
+      $('#menubar').css('margin-left', -$('#menubar').width());
+      $('#menubar').css('display', 'none');
       $('#heading').css('padding-top', $(window).height()/2.4-$('#heading').height()/2);
       $('#heading').css('margin-left', $(window).width()/2-$('#heading').width()/2);
       $('.album_cover').css('width', $(window).height()/3);
